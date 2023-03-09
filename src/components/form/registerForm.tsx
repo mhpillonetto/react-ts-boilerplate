acf01a31626479e251ffce9bf360bed2875d5332
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-09 19:51:37 -0300

    add: basic form funcionality

--- src/components/form/registerForm.tsx
+++ src/components/form/registerForm.tsx
@@ -1,24 +1,110 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+
+interface IFormState {
+  username: string
+  email: string
+  password: string
+}
 
 const RegisterForm = () => {
-  return (
-      <form>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1"/>
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
 
+  const [formState, setFormState] = useState<IFormState>({
+    username: "",
+    email: "",
+    password: ""
+  })
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const targetInput = event.currentTarget
+
+    const {value, name} = targetInput
+
+    setFormState({
+      ... formState,
+      [name]: value,
+    })
+  }, [formState])
+
+
+
+  const handleSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const { username, email, password } = formState;
+
+    if (!email || !username || !password) {
+      window.alert("Fill all the required fields")
+
+      setIsLoggedIn(false)
+
+      return
+    }
+
+    setIsLoggedIn(true)
+
+  },[formState]);
+
+
+  if (isLoggedIn) {
+    return(
+      <div>
+        <h1>New Trainer Created :)</h1>
+      </div>
+    )
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="exampleInputUsername" className="form-label">
+          Username
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          name="username"
+          required
+          value={formState.username}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          name="email"
+          required
+          value={formState.email}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          name="password"
+          required
+          value={formState.password}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
   )
 }
 
